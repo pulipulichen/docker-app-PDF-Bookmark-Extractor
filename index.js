b033165aa4f207c4a7de0c802cf5f4810b340a35
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-01-05 05:20:18 +0800

    Thu Jan  5 05:20:18 AM CST 2023

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const ShellSpawn = require('./app/lib/ShellSpawn')
 const GetExistedArgv = require('./app/lib/GetExistedArgv')
 const SetDockerComposeYML = require('./app/lib/SetDockerComposeYML')
 
+const path = require('path')
+const fs = require('fs')
+
 let main = async function () {
   // 1. 先取得輸入檔案的列表
   let files = GetExistedArgv()
@@ -13,8 +16,16 @@ let main = async function () {
       continue
     }
     
-    SetDockerComposeYML(file)
+		let filename = path.basename(file)
+		let tempFilename = Math.floor(Math.random() * 100000) + '.pdf'
+		let tempFilePath = `/input/` + tempFilename
+		fs.renameSync(file, tempFilePath)
+
+
+    SetDockerComposeYML(tempFilePath)
     await ShellSpawn('docker-compose up')
+		fs.renameSync(tempFilePath, file)
+		fs.renameSync(`/input/` + tempFilename.slice(0, -4) + '.txt', `/input/` + filename.slice(0, -4) + '.txt')
   }
 }
 
